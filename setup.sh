5329b0b57e1c635021ec98a0ec95b2b2410578a8
Adam Mayer <phooky@gmail.com>
2012-06-11 15:34:13 -0400

    Instruct pip to use mirrors when pypi goes down (COMPLETELY INCONCEIVABLE)

--- setup.sh
+++ setup.sh
@@ -6,4 +6,4 @@ then
 fi
 
 . virtualenv/bin/activate
-pip install coverage doxypy lockfile python-daemon unittest-xml-reporting
+pip install --use-mirrors coverage doxypy lockfile python-daemon unittest-xml-reporting
