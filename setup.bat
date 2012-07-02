beaf0a4c3a5acea9f5be9215b0dca559e94376c8
Filipp Gelman <filipp@makerbot.com>
2012-07-02 17:18:36 -0400

    Translated setup.sh to setup.bat for windows

---
+++ setup.bat
@@ -0,0 +1,12 @@
+@ECHO OFF
+IF NOT EXIST virtualenv GOTO DIRNOTEXISTS
+:DIREXISTS
+virtualenv\scripts\activate
+pip install --use-mirrors argparse coverage doxypy lockfile pyserial unittest-xml-reporting
+GOTO DONE
+
+:DIRNOTEXISTS
+python virtualenv.py virtualenv
+GOTO DIREXISTS
+
+:DONE
