74c6f3dd762389a8f2e4a563a77d0e2adadebf66
bmatzelle <bmatzelle@gmail.com>
2012-07-13 09:22:29 -0400

    * Added "CALL" before virtualenv activate so that the script doesn't stall out. * Switched the .conf file to the correct server file.

--- restart.bat
+++ restart.bat
@@ -1,5 +1,5 @@
 @ECHO OFF
 CALL stop.bat
 IF EXIST conveyord.log DEL /F /Q conveyord.log
-IF EXIST virtualenv virtualenv\Scripts\activate
-python conveyor_service.py -l DEBUG -c conveyor-user.conf --nofork
+IF EXIST virtualenv CALL virtualenv\Scripts\activate
+python conveyor_service.py -l DEBUG -c conveyor-user-server-win32.conf --nofork
