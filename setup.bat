272d4b0c08cf3fa133a1c34102d99f3c67127fe1
Filipp Gelman <filipp@makerbot.com>
2012-07-27 12:59:42 -0400

    Updated windows setup.bat to match setup.sh

--- setup.bat
+++ setup.bat
@@ -4,8 +4,12 @@ IF NOT EXIST virtualenv GOTO DIRNOTEXISTS
 :DIREXISTS
 CALL virtualenv\scripts\activate
 
+SET PYTHONPATH=%CD%\submodule\s3g;%CD%\src\main\python;%PYTHONPATH%
+
+REM EXIT /B 0
+
 pip install -q --use-mirrors argparse coverage doxypy lockfile mock pyserial unittest-xml-reporting
-easy_install submodules/conveyor_bins/pyserial-2.7_mb-py2.7.egg
+easy_install submodule\conveyor_bins\pyserial-2.7_mb-py2.7.egg
 GOTO DONE
 
 :DIRNOTEXISTS
