d3c46859e41360a3c7f136c9f5f9bfc1d3adf7bd
Far McKon <farmckon@gmail.com>
2012-07-30 17:07:53 -0400

    fixed path problem in setup.sh. I forget I have smart-path-sep setup on my home system

--- setup.sh
+++ setup.sh
@@ -7,5 +7,5 @@ fi
 
 . virtualenv/bin/activate
 pip install --use-mirrors argparse coverage doxypy lockfile python-daemon unittest-xml-reporting
-easy_install submodule\conveyor_bins\pyserial-2.7_mb-py2.7.egg
+easy_install submodule/conveyor_bins/pyserial-2.7_mb-py2.7.egg
 export PYTHONPATH=./submodule/s3g:./src/main/python:$PYTHONPATH
