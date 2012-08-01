f99c9c89f676cf79ede2c6235d53c1141c908355
Ted <Ted@Makerbot.com>
2012-08-01 16:29:25 -0400

    Some changes to virtualenv to get automated builds working.

--- setup.bat
+++ setup.bat
@@ -8,7 +8,7 @@ SET PYTHONPATH=%CD%\submodule\s3g;%CD%\src\main\python;%PYTHONPATH%
 
 REM EXIT /B 0
 
-pip install -q --use-mirrors argparse coverage doxypy lockfile mock pyserial unittest-xml-reporting
+pip install -q --use-mirrors argparse coverage doxypy mock pyserial unittest-xml-reporting unittest2
 easy_install submodule\conveyor_bins\pyserial-2.7_mb-py2.7.egg
 GOTO DONE
 
--- setup.sh
+++ setup.sh
@@ -6,6 +6,6 @@ then
 fi
 
 . virtualenv/bin/activate
-pip install --use-mirrors argparse coverage doxypy lockfile python-daemon unittest-xml-reporting
+pip install --use-mirrors argparse coverage doxypy mock lockfile python-daemon unittest-xml-reporting unittest2
 easy_install submodule/conveyor_bins/pyserial-2.7_mb-py2.7.egg
 export PYTHONPATH=./submodule/s3g:./src/main/python:$PYTHONPATH
