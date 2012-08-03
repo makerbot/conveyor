b348056e91a3a2fe3b76ea8edc0889ec695e57c4
Far McKon <FarMcKon@gmail.com>
2012-08-03 14:23:42 -0400

    updated to include mock in python pip install

--- setup.sh
+++ setup.sh
@@ -6,5 +6,5 @@ then
 fi
 
 . virtualenv/bin/activate
-pip install --use-mirrors argparse coverage doxypy lockfile pyserial python-daemon unittest-xml-reporting
+pip install --use-mirrors argparse coverage doxypy lockfile pyserial python-daemon unittest-xml-reporting mock
 export PYTHONPATH=./:./submodule/s3g:$PYTHONPATH
