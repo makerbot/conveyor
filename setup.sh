0bec4c0a8f7a658d65d25f9d9bbce02b8e30299c
Joe Sadusk <joseph.sadusk@makerbot.com>
2012-08-21 15:23:29 -0400

    setup.sh now allows you to specify the version of python to create your virtualenv from.  Should also pull in an appropriate version of pyserial

--- setup.sh
+++ setup.sh
@@ -1,12 +1,22 @@
 #! /bin/sh
 
+PYVERSION=$1
+
+if [ -z $PYVERSION ]
+then
+    SERIALVERSION=`python --version 2>&1 | sed s/'Python '//|sed s/\.[0-9]*$//`
+else
+    SERIALVERSION=$PYVERSION
+fi
+ 
 if [ ! -d virtualenv/ ]
 then
-	python virtualenv.py virtualenv
+	python$PYVERSION virtualenv.py virtualenv
 fi
 
 . virtualenv/bin/activate
-pip install -q --use-mirrors coverage doxypy mock lockfile python-daemon unittest-xml-reporting argparse
-easy_install -q submodule/conveyor_bins/pyserial-2.7_mb2.1-py2.7.egg
+pip install -q --upgrade setuptools
+pip install -q --use-mirrors coverage doxypy mock lockfile python-daemon unittest-xml-reporting argparse unittest2
+easy_install -q submodule/conveyor_bins/pyserial-2.7_mb2.1-py$SERIALVERSION.egg
 
 export PYTHONPATH=./submodule/s3g:./src/main/python:$PYTHONPATH
