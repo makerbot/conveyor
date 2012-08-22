bc71f3b773c3077939ae1829db9d8f51799306c3
Joe Sadusk <joseph.sadusk@makerbot.com>
2012-08-22 16:25:28 -0400

    setup.sh works on a mac without xcode.  It adds a binary path with install_name_tool to its path, and installs distutils source if needed

--- setup.sh
+++ setup.sh
@@ -1,22 +1,41 @@
-#! /bin/sh
+#!/bin/bash
 
 PYVERSION=$1
+BASEDIR=`echo $0|sed 's|/.*||'`
 
 if [ -z $PYVERSION ]
 then
-    SERIALVERSION=`python --version 2>&1 | sed s/'Python '//|sed s/\.[0-9]*$//`
+    PYVERSION=`python --version 2>&1 | sed s/'Python '//|sed s/\.[0-9]*$//`
+    echo "python version is $PYVERSION"
 else
-    SERIALVERSION=$PYVERSION
+    PYBINVERSION=$PYVERSION
 fi
- 
+
+UNAME=`uname`
+MAC_DISTUTILS=/System/Library/Frameworks/Python.framework/Versions/$PYVERSION/lib/python$PYVERSION/distutils/__init__.py
+if [ "$UNAME" == "Darwin" ]
+then
+    MACVER=`system_profiler SPSoftwareDataType |grep '^ *System Version:' |sed 's/.*Mac OS X //' | sed 's/\.[^.]*$//'`
+
+    export PATH=$PATH:$BASEDIR/submodule/conveyor_bins/mac/$MACVER
+
+    if [ ! -f $MAC_DISTUTILS ]
+    then
+	sudo cp mac/$PYVERSION/distutils/__init__.py $MAC_DISTUTILS
+    fi
+fi
+
 if [ ! -d virtualenv/ ]
 then
-	python$PYVERSION virtualenv.py virtualenv
+	python$PYBINVERSION virtualenv.py virtualenv
 fi
 
 . virtualenv/bin/activate
+echo "Upgrading setuptools"
 pip install -q --upgrade setuptools
+echo "Installing modules"
 pip install -q --use-mirrors coverage doxypy mock lockfile python-daemon unittest-xml-reporting argparse unittest2
-easy_install -q submodule/conveyor_bins/pyserial-2.7_mb2.1-py$SERIALVERSION.egg
+echo "Installing pyserial egg"
+easy_install -q submodule/conveyor_bins/pyserial-2.7_mb2.1-py$PYVERSION.egg
 
 export PYTHONPATH=./submodule/s3g:./src/main/python:$PYTHONPATH
