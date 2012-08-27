36c828523f514a7b1a5f54cbe8c3306f20e8c0f3
Dave Sayles <sayles.dave@gmail.com>
2012-08-27 17:06:38 -0400

    added eeprom read and write to S3gDriver

--- src/main/python/conveyor/printer/s3g.py
+++ src/main/python/conveyor/printer/s3g.py
@@ -301,3 +301,31 @@ class S3gDriver(object):
                 self._genericprint(
                     profile, buildname, writer, False, 5.0,
                     gcodepath, skip_start_end, task)
+
+    def write_eeprom(
+        self, inputpath, fp, directory=None):
+            s = self.create_s3g_from_fp(fp)
+            version = str(s.get_version())
+            version = version.replace('0', '.')
+            eeprom_writer = makerbot_driver.EEPROM.EepromWriter.factory(s, version, directory)
+            import json
+            with open(inputpath) as f:
+                eeprom_values = json.load(f)
+            eeprom_writer.write_entire_map(eeprom_values)
+
+    def read_eeprom(
+        self, outputpath, fp, directory=None):
+            s = self.create_s3g_from_fp(fp)
+            version = str(s.get_version())
+            version = version.replace('0', '.')
+            eeprom_reader = makerbot_driver.EEPROM.EepromReader.factory(s, version, directory)
+            the_map = eeprom_reader.read_entire_map()
+            import json
+            dump = json.dumps(the_map, sort_keys=True, indent=2)
+            with open(outputpath, 'w') as f:
+                f.write(dump)
+
+    def create_s3g_from_fp(self, fp):
+        s = makerbot_driver.s3g()
+        s.writer = makerbot_driver.Writer.StreamWriter(fp)
+        return s
