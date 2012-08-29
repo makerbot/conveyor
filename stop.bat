4370b13ee7a3be1a987320f04863597fdd1deccb
Filipp Gelman <filipp@makerbot.com>
2012-08-29 09:51:02 -0400

    stop.bat smarter about moving log files

--- stop.bat
+++ stop.bat
@@ -1,24 +1,28 @@
-@ECHO OFF
+REM @ECHO OFF
 
 SET CURDATE=%DATE:/=_%
-SET CURDATEFULL=%CURDATE: =_%
+SET CURDATENC=%CURDATE::=_%
+SET CURDATEFULL=%CURDATENC: =_%
 
 SET CURTIME=%TIME:~0,-6%
-SET CURTIMEFULL=%CURTIME::=_%
+SET CURTIMENC=%CURTIME::=_%
+SET CURTIMEFULL=%CURTIMENC: =%
 
 SET DATETIMEPREFIX=%CURDATEFULL%_%CURTIMEFULL%_
 
 IF EXIST conveyord.avail.lock GOTO KILLSUCCEED
 ECHO No such file or directory: conveyord.avail.lock
-EXIT /B 1
-GOTO DONE
+GOTO MOVELOG
 
 :KILLSUCCEED
 
 REM delete conveyord.avail.lock
 DEL /F /Q conveyord.avail.lock
 
+:MOVELOG
+
 REM move conveyord.log to DATE_conveyord.log
+IF NOT EXIST conveyord.log GOTO FAILED
 MOVE /Y conveyord.log %DATETIMEPREFIX%conveyord.log
 
 
@@ -27,3 +31,8 @@ REM There is no need to do this
 REM IF EXIST virtualenv call virtualenv\Scripts\deactivate
 
 EXIT /B 0
+
+:FAILED
+
+EXIT /B 1
+
