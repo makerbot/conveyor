e3f3d64d096a91ed89635c79220280b109779f16
Dave Sayles <sayles.dave@gmail.com>
2012-11-09 20:13:38 -0500

    tentatively fixed dualstrusion 	* Skeinforge should now get woven together correctly 	* Toolchange now emits a G1 comand, which will move the Gantry to its previous position at the next position's layer height.  Its movement speed is dependent on the last speed used.

--- src/main/python/conveyor/dualstrusion.py
+++ src/main/python/conveyor/dualstrusion.py
@@ -1,12 +1,31 @@
+from __future__ import print_function
+
+import os
+import sys
+path = os.path.join(
+    os.path.abspath(os.path.dirname(__file__)),
+    '../../../../../',
+    's3g',
+)
+sys.path.insert(0, path)
+
 import re
 import mock
 import unittest
+try:
+    import makerbot_driver
+except ImportError:
+    print("Cannot find makerbot_driver.  Expected at: %s.  Trying to continue anyway..." % (path))
 
 class DualstrusionWeaver(object):
 
     def __init__(self, tool_0_codes, tool_1_codes):
         self.tool_0_codes = tool_0_codes
         self.tool_1_codes = tool_1_codes
+        self.last_position_0 = None
+        self.next_position_0 = None
+        self.last_position_1 = None
+        self.next_position_1 = None
         self.last_used_codes = self.tool_0_codes
         tool_0_length = len(self.tool_0_codes.gcodes)
         tool_1_length = len(self.tool_1_codes.gcodes)
@@ -22,23 +41,71 @@ class DualstrusionWeaver(object):
         commands = []
         if tool_codes == self.tool_0_codes:
             tool = 0
+            transition_codes = self.create_transition_location(self.last_position_0, self.next_position_0)
         else:
             tool = 1
+            transition_codes = self.create_transition_location(self.last_position_1, self.next_position_1)
         toolchange = "M135 T%i\n" % (tool)
         commands.append(toolchange)
+        commands.extend(transition_codes)
         return commands
 
     def combine_codes(self, callback=None):
         while len(self.tool_0_codes.gcodes) is not 0 or len(self.tool_1_codes.gcodes) is not 0:
             next_gcode_obj = self.get_next_code_list()
-            toolchange_codes = self.get_toolchange_commands(next_gcode_obj)
             next_layer = self.get_next_layer(next_gcode_obj)
+            self.set_next_location(next_layer, next_gcode_obj)
+            toolchange_codes = self.get_toolchange_commands(next_gcode_obj)
             self.new_codes.extend(toolchange_codes)
             self.new_codes.extend(next_layer)
+            self.set_last_location(next_layer, next_gcode_obj)
         if callback:
             percent = len(self.longest_list) / float(self.total_length)
             callback(percent)
         return self.new_codes
+
+    @staticmethod
+    def create_transition_location(previous_point, next_point):
+        all_codes = []
+        if previous_point:
+            prev_codes = makerbot_driver.Gcode.parse_command(previous_point)[0]
+            # Attribute errors mean these commands are None.  We will try to recover though to produce something usable
+            try:
+                next_codes = makerbot_driver.Gcode.parse_command(next_point)[0]
+            except AttributeError as e:
+                next_codes = {}
+            trans_code = "G1"
+            for axis in ['X', 'Y']:
+                if axis in prev_codes:
+                    trans_code += " %s"  % (axis) + str(prev_codes[axis])
+            for axis in ['Z']:
+                if axis in next_codes:
+                    trans_code += " %s"  % (axis) + str(next_codes[axis])
+            trans_code += '\n'
+            if 'X' in trans_code or 'Y' in trans_code or 'Z' in trans_code:
+                all_codes.append(trans_code)
+        return all_codes
+
+    def set_next_location(self, codes, gcode_obj):
+        g1_regex = re.compile("[gG]1.*?[zZ][-]?([\d]*\.?[\d]+)")
+        for code in codes:
+            if re.match(g1_regex, code):
+                if gcode_obj == self.tool_0_codes:
+                    self.next_position_0 = code
+                else:
+                    self.next_position_1 = code
+                break
+
+    def set_last_location(self, codes, gcode_obj):
+        g1_regex = re.compile("[gG]1.*?[xXyY][-]?([\d]*\.?[\d]+)")
+        codes.reverse()
+        for code in codes:
+            if re.match(g1_regex, code):
+                if gcode_obj == self.tool_0_codes:
+                    self.last_position_0 = code
+                else:
+                    self.last_position_1 = code
+                break
 
     def get_next_code_list(self):
         tool_0_height = self.tool_0_codes.peek_next_layer_height()
@@ -56,7 +123,7 @@ class GcodeObject(object):
 
     def __init__(self, gcodes=[]):
         self.gcodes = gcodes
-        self.skeinforge_tag = re.compile("</layer>")
+        self.skeinforge_tag = re.compile("\(</layer>\)")
         self.miraclegrue_tag = re.compile("\(Slice (\d+), (\d+) Extruder\)")
         self.layer_height_regex = re.compile("[gG]1.*?[zZ]([\d]*\.?[\d]+)")
         self.last_layer_height = 0
@@ -163,11 +230,11 @@ class TestGcodeObject(unittest.TestCase):
             "G92 X0 Y0 Z50",
             "M99",
             "(some interesting comments)",
-            "</layer>",
+            "(</layer>)",
             "<layer>",
             "G1 X1 Y2 Z3",
             "G92 X100 Y100 Z100 A100",
-            "</layer>",
+            "(</layer>)",
         ]
         self.gcode_obj.gcodes = gcodes[:]
         expected_layer = [
@@ -176,7 +243,7 @@ class TestGcodeObject(unittest.TestCase):
             "G92 X0 Y0 Z50",
             "M99",
             "(some interesting comments)",
-            "</layer>"
+            "(</layer>)"
         ]
         expected_leftovers = gcodes[6:]
         self.assertEqual(expected_layer, self.gcode_obj.get_next_layer())
@@ -241,12 +308,12 @@ class TestDualstrusionWeaver(unittest.TestCase):
             "M132",
             "G92 X0 Y0 Z0 A0 B0",
             "G1 X50 Y50 Z1.05",
-            "</layer>",
+            "(</layer>)",
             "<layer>",
             "M132",
             "G1 X0 Y0 Z1.5",
-            "G92 X99 Y99:",
-            "</layer>",
+            "G92 X99 Y99",
+            "(</layer>)",
         ]
         t1_codes = [
             "G1 X1 Y2 Z1.05",
@@ -266,7 +333,7 @@ class TestDualstrusionWeaver(unittest.TestCase):
             "M132",
             "G92 X0 Y0 Z0 A0 B0",
             "G1 X50 Y50 Z1.05",
-            "</layer>",
+            "(</layer>)",
             "M135 T1\n",
             "G1 X1 Y2 Z1.05",
             "G1 X59 Y58",
@@ -274,17 +341,19 @@ class TestDualstrusionWeaver(unittest.TestCase):
             "G92",
             "(Slice 54, 3 Extruder)",
             "M135 T1\n",
+            "G1 X59 Y58 Z1.5\n",
             "G1 X1 Y2 Z1.5",
             "G99",
             "M101",
             "M105",
             "(Slice 55, 3 Extruder)",
             "M135 T0\n",
+            "G1 X50 Y50 Z1.5\n",
             "<layer>",
             "M132",
             "G1 X0 Y0 Z1.5",
-            "G92 X99 Y99:",
-            "</layer>",
+            "G92 X99 Y99",
+            "(</layer>)",
         ]
         tool_0_codes = GcodeObject(gcodes=t0_codes)
         tool_1_codes = GcodeObject(gcodes=t1_codes)
@@ -298,12 +367,12 @@ class TestDualstrusionWeaver(unittest.TestCase):
             "M132",
             "G92 X0 Y0 Z0 A0 B0",
             "G1 X50 Y50 Z50",
-            "</layer>",
+            "(</layer>)",
             "<layer>",
             "M132",
             "G1 X0 Y0 Z0",
             "G92 X99 Y99:",
-            "</layer>",
+            "(</layer>)",
             ]
         expected_t0_codes = t0_codes[:5]
         expected_leftovers = t0_codes[5:]
@@ -317,13 +386,13 @@ class TestDualstrusionWeaver(unittest.TestCase):
         codes = [
             "<layer>",
             "G1 Z1.05",
-            "</layer>",
+            "(</layer>)",
             "<layer>",
             "G1 Z1.5",
-            "</layer>",
+            "(</layer>)",
             "<layer>",
             "G1 Z2.05",
-            "</layer>",
+            "(</layer>)",
         ]
             
         t0_codes = GcodeObject(codes[:])
@@ -346,7 +415,117 @@ class TestDualstrusionWeaver(unittest.TestCase):
             "M135 T0\n"
         ]
         self.assertEqual(expected_toolchange_command, weaver.get_toolchange_commands(t0_codes))
-        
+
+    def test_get_toolchange_commands_with_last_used_t0(self):
+        t0_codes = GcodeObject([])
+        t1_codes = GcodeObject([])
+        weaver = DualstrusionWeaver(t0_codes, t1_codes)
+        weaver.last_position_0 = 'G1 X50 Y50'
+        weaver.next_position_0 = 'G1 X0 Y0 Z500'
+        weaver.last_position_1 = 'G1 X100 Y100'
+        weaver.next_position_1 = 'G1 X0 Y0 Z400'
+        expected_toolchange_command = [
+            "M135 T0\n",
+            "G1 X50 Y50 Z500\n",
+        ]
+        self.assertEqual(expected_toolchange_command, weaver.get_toolchange_commands(t0_codes))
+
+    def test_get_toolchange_commands_with_last_used_t1(self):
+        t0_codes = GcodeObject([])
+        t1_codes = GcodeObject([])
+        weaver = DualstrusionWeaver(t0_codes, t1_codes)
+        weaver.last_position_0 = 'G1 X50 Y50'
+        weaver.next_position_0 = 'G1 X0 Y0 Z500'
+        weaver.last_position_1 = 'G1 X100 Y100'
+        weaver.next_position_1 = 'G1 X0 Y0 Z400'
+        expected_toolchange_command = [
+            "M135 T1\n",
+            "G1 X100 Y100 Z400\n",
+        ]
+        self.assertEqual(expected_toolchange_command, weaver.get_toolchange_commands(t1_codes))
+
+    def test_get_toolchange_commands_with_last_used_t0_last_none(self):
+        t0_codes = GcodeObject([])
+        t1_codes = GcodeObject([])
+        weaver = DualstrusionWeaver(t0_codes, t1_codes)
+        weaver.last_position_0 = None
+        weaver.next_position_0 = 'G1 X0 Y0 Z500'
+        weaver.last_position_1 = 'G1 X100 Y100'
+        weaver.next_position_1 = 'G1 X0 Y0 Z400'
+        expected_toolchange_command = [
+            "M135 T0\n",
+        ]
+        self.assertEqual(expected_toolchange_command, weaver.get_toolchange_commands(t0_codes))
+
+    def test_get_toolchange_commands_with_last_used_t0_next_is_none(self):
+        t0_codes = GcodeObject([])
+        t1_codes = GcodeObject([])
+        weaver = DualstrusionWeaver(t0_codes, t1_codes)
+        weaver.last_position_0 = 'G1 X50 Y51'
+        weaver.next_position_0 = None
+        weaver.last_position_1 = 'G1 X100 Y100'
+        weaver.next_position_1 = 'G1 X0 Y0 Z400'
+        expected_toolchange_command = [
+            "M135 T0\n",
+            "G1 X50 Y51\n",
+        ]
+        self.assertEqual(expected_toolchange_command, weaver.get_toolchange_commands(t0_codes))
+
+    def test_get_toolchange_commands_with_last_used_t1_last_is_none(self):
+        t0_codes = GcodeObject([])
+        t1_codes = GcodeObject([])
+        weaver = DualstrusionWeaver(t0_codes, t1_codes)
+        weaver.last_position_0 = 'G1 X50 Y50'
+        weaver.next_position_0 = 'G1 X0 Y0 Z500'
+        weaver.last_position_1 = None
+        weaver.next_position_1 = 'G1 X0 Y0 Z400'
+        expected_toolchange_command = [
+            "M135 T1\n",
+        ]
+        self.assertEqual(expected_toolchange_command, weaver.get_toolchange_commands(t1_codes))
+
+    def test_get_toolchange_commands_with_last_used_t1_next_is_none(self):
+        t0_codes = GcodeObject([])
+        t1_codes = GcodeObject([])
+        weaver = DualstrusionWeaver(t0_codes, t1_codes)
+        weaver.last_position_0 = 'G1 X50 Y50'
+        weaver.next_position_0 = 'G1 X0 Y0 Z500'
+        weaver.last_position_1 = 'G1 X100 Y100'
+        weaver.next_position_1 = None
+        expected_toolchange_command = [
+            "M135 T1\n",
+            "G1 X100 Y100\n",
+        ]
+        self.assertEqual(expected_toolchange_command, weaver.get_toolchange_commands(t1_codes))
+                        
+    def test_get_last_location_no_codes(self):
+        codes_t0 = [
+            "G1 X0 Y0", 
+            "G1 X-50 Y-50",
+        ]
+        codes_t1 = [
+            "G1 X0 Y0", 
+            "G1 X100 Y100",
+        ]
+        weaver = DualstrusionWeaver(GcodeObject(codes_t0), GcodeObject(codes_t1))
+        weaver.set_last_location(codes_t0[:], weaver.tool_0_codes)
+        self.assertEqual(weaver.last_position_0, codes_t0[1])
+        self.assertEqual(weaver.last_position_1, None)
+        weaver.set_last_location(codes_t1[:], weaver.tool_1_codes)
+        self.assertEqual(weaver.last_position_1, codes_t1[1])
+        self.assertEqual(weaver.last_position_0, codes_t0[1])
+
+    def test_create_transition_code(self):
+        cases = [
+            ['G1 X50.05 Y51 Z0', 'G1 X99 Y99 Z100', ['G1 X50.05 Y51 Z100\n']],
+            ['G1 Y50 Z0', 'G1 X12 Y13 Z50', ['G1 Y50 Z50\n']],
+            ['G1 Z0', 'G1 X12 Y13 Z50', ['G1 Z50\n']],
+            ['G1 Z0', 'G1 X12 Y12', []],
+            [None, 'G1 Z100', []],
+            ['G1 X50 Y51', None, ['G1 X50 Y51\n']],
+        ]
+        for case in cases:
+            self.assertEqual(case[2], DualstrusionWeaver.create_transition_location(case[0], case[1]))
 
 
 if __name__ == "__main__":
