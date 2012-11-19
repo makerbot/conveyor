77cc7a3c4b6f12789101d6d7418c8e9f9b5e31e5
Joe Sadusk <joseph.sadusk@makerbot.com>
2012-11-19 18:22:53 -0500

    Fixed offline install

--- setup.bat
+++ setup.bat
@@ -9,18 +9,18 @@ SET PYTHONPATH=%CD%\submodule\s3g;%CD%\src\main\python;%PYTHONPATH%
 REM EXIT /B 0
 
 easy_install -q submodule\conveyor_bins\python\pyserial-2.7_mb2.1-py2.7.egg
-easy_install -q submodule\conveyor_bins\python\mock-1.0.1-py$PYVERSION.egg
-easy_install -q submodule\conveyor_bins\python\lockfile-0.9.1-py$PYVERSION.egg
-easy_install -q submodule\conveyor_bins\python\python_daemon-1.5.5-py$PYVERSION.egg
-easy_install -q submodule\conveyor_bins\python\argparse-1.2.1-py$PYVERSION.egg
-easy_install -q submodule\conveyor_bins\python\unittest2-0.5.1-py$PYVERSION.egg
-easy_install -q submodule\conveyor_bins\python\pyserial-2.7_mb2.1-py$PYVERSION.egg
+easy_install -q submodule\conveyor_bins\python\mock-1.0.1-py2.7.egg
+easy_install -q submodule\conveyor_bins\python\lockfile-0.9.1-py2.7.egg
+easy_install -q submodule\conveyor_bins\python\python_daemon-1.5.5-py2.7.egg
+easy_install -q submodule\conveyor_bins\python\argparse-1.2.1-py2.7.egg
+easy_install -q submodule\conveyor_bins\python\unittest2-0.5.1-py2.7.egg
+easy_install -q submodule\conveyor_bins\python\pyserial-2.7_mb2.1-py2.7.egg
 
 GOTO DONE
 
 
 :DIRNOTEXISTS
-python virtualenv.py virtualenv
+python virtualenv.py --extra-search-dir=submodule/conveyor_bins/python --never-download virtualenv
 GOTO DIREXISTS
 
 :DONE
