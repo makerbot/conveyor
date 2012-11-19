0770593580da4bd576ebcb41c03f522dfd006daf
Joe Sadusk <joseph.sadusk@makerbot.com>
2012-11-19 17:05:42 -0500

    Windows installer offline mode

--- setup.bat
+++ setup.bat
@@ -8,8 +8,14 @@ SET PYTHONPATH=%CD%\submodule\s3g;%CD%\src\main\python;%PYTHONPATH%
 
 REM EXIT /B 0
 
-pip install -q --use-mirrors coverage doxypy lockfile mock unittest-xml-reporting argparse
-easy_install -q submodule\conveyor_bins\pyserial-2.7_mb2.1-py2.7.egg
+easy_install -q submodule\conveyor_bins\python\pyserial-2.7_mb2.1-py2.7.egg
+easy_install -q submodule\conveyor_bins\python\mock-1.0.1-py$PYVERSION.egg
+easy_install -q submodule\conveyor_bins\python\lockfile-0.9.1-py$PYVERSION.egg
+easy_install -q submodule\conveyor_bins\python\python_daemon-1.5.5-py$PYVERSION.egg
+easy_install -q submodule\conveyor_bins\python\argparse-1.2.1-py$PYVERSION.egg
+easy_install -q submodule\conveyor_bins\python\unittest2-0.5.1-py$PYVERSION.egg
+easy_install -q submodule\conveyor_bins\python\pyserial-2.7_mb2.1-py$PYVERSION.egg
+
 GOTO DONE
 
 
