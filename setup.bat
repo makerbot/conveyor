f20324c46f10094a24c7e7a950dbd64ed86f2e3d
Joe Sadusk <joseph.sadusk@makerbot.com>
2012-11-20 16:53:44 -0500

    Make setup.bat have an optional python binary parameter, fix python_daemon version

--- setup.bat
+++ setup.bat
@@ -11,7 +11,7 @@ REM EXIT /B 0
 easy_install -q submodule\conveyor_bins\python\pyserial-2.7_mb2.1-py2.7.egg
 easy_install -q submodule\conveyor_bins\python\mock-1.0.1-py2.7.egg
 easy_install -q submodule\conveyor_bins\python\lockfile-0.9.1-py2.7.egg
-easy_install -q submodule\conveyor_bins\python\python_daemon-1.5.5-py2.7.egg
+easy_install -q submodule\conveyor_bins\python\python_daemon-1.6-py2.7.egg
 easy_install -q submodule\conveyor_bins\python\argparse-1.2.1-py2.7.egg
 easy_install -q submodule\conveyor_bins\python\unittest2-0.5.1-py2.7.egg
 easy_install -q submodule\conveyor_bins\python\pyserial-2.7_mb2.1-py2.7.egg
@@ -20,7 +20,16 @@ GOTO DONE
 
 
 :DIRNOTEXISTS
-python virtualenv.py --extra-search-dir=submodule/conveyor_bins/python --never-download virtualenv
+
+IF "%1" == "" GOTO DEFAULTPY
+set PYTHON=%1
+GOTO VIRTUALENV
+
+:DEFAULTPY
+set PYTHON=python
+
+:VIRTUALENV
+%PYTHON% virtualenv.py --extra-search-dir=submodule/conveyor_bins/python --never-download virtualenv
 GOTO DIREXISTS
 
 :DONE
