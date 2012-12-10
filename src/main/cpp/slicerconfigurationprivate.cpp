e734ae8b9b98c9e37a4bc080572b64797f15616c
Dave Sayles <sayles.dave@gmail.com>
2012-12-10 17:22:35 -0500

    Changed infill for high profile

--- src/main/cpp/slicerconfigurationprivate.cpp
+++ src/main/cpp/slicerconfigurationprivate.cpp
@@ -32,6 +32,7 @@ namespace conveyor
         case SlicerConfiguration::HighQuality:
             config->setSlicer(SlicerConfiguration::Skeinforge);
             config->setLayerHeight(.1);
+            config->setInfill(0.15);
             break;
 
         case SlicerConfiguration::SkeinforgeDefaults:
