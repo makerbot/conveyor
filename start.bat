ff19ddf0f31fe5d7550b2de5eba71c476b572b68
Ted <Ted@Makerbot.com>
2013-01-25 17:13:33 -0500

    updating start.bat: use user-facing .conf, not dev-facing .conf

--- start.bat
+++ start.bat
@@ -7,5 +7,6 @@ SET CONVEYOR_SYNC_FILE=conveyor.pid
 IF EXIST %CONVEYOR_SYNC_FILE% DEL /F /Q %CONVEYOR_SYNC_FILE%
 
 ECHO Starting conveyor backend from start.bat
-python conveyor_service.py -c conveyor-win32.conf -l INFO
+
+python conveyor_service.py -c conveyor.conf -l INFO
 
