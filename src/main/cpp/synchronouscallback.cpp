ccf78e82c0f0ae9f07e185f2930f44e4b1f78fa6
Nicholas Bishop <nicholasbishop@gmail.com>
2013-01-31 18:50:04 -0500

    Improve console error print in SynchronousCallback

--- src/main/cpp/synchronouscallback.cpp
+++ src/main/cpp/synchronouscallback.cpp
@@ -1,9 +1,9 @@
 // vim:cindent:cino=\:0:et:fenc=utf-8:ff=unix:sw=4:ts=4:
 
-#include <QDebug>
 #include <QMutex>
 #include <QMutexLocker>
 #include <QWaitCondition>
+#include <cstdio>
 #include <stdexcept>
 #include <string>
 
@@ -85,8 +85,13 @@ namespace conveyor
             std::string const message (error["message"].asString ());
             Json::Value const data (error["data"]);
 
-            qDebug() << "SynchronousCallback error:";
-            qDebug() << response.toStyledString().c_str();
+            fprintf(stderr,
+                "Error invoking \"%s\"\n"
+                "This may indicate mismatched "
+                "Conveyor and MakerWare versions,\n"
+                "check your Conveyor and MakerWare "
+                "logs for more information.\n\n",
+                methodName.c_str());
 
             throw JsonRpcException (methodName, params, code, message, data);
         }
