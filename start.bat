b6f614dd1392e34f19d640b96a0eea5270108566
Ted <Ted@Makerbot.com>
2013-02-01 14:29:54 -0500

    some changes for the new windows install structure.

--- start.bat
+++ start.bat
@@ -8,5 +8,5 @@ IF EXIST %CONVEYOR_SYNC_FILE% DEL /F /Q %CONVEYOR_SYNC_FILE%
 
 ECHO Starting conveyor backend from start.bat
 
-python conveyor_service.py -c conveyor.conf -l INFO
+python -m conveyor.service -c conveyor.conf
 
