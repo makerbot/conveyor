32ea15e270b33c065ecf9b62d277e123c1e8640c
Matthew W. Samsonoff <matthew.samsonoff@makerbot.com>
2013-02-04 18:08:28 -0500

    Fix a stupid typo in start.bat.

--- start.bat
+++ start.bat
@@ -6,5 +6,5 @@ IF EXIST %CONVEYOR_SYNC_FILE% DEL /F /Q %CONVEYOR_SYNC_FILE%
 
 ECHO Starting conveyor backend from start.bat
 
-virtualenv\Scripts\python.exe -m conveyor.service -c conveyor.conf
+virtualenv\Scripts\python.exe -m conveyor.server -c conveyor.conf
 
