49b2eb31b4fe646504da6422f4dd40707b7c0e9b
Matthew W. Samsonoff <matthew.samsonoff@makerbot.com>
2013-02-05 15:12:48 -0500

    More fixes for the OSX default paths.

--- src/main/python/conveyor/platform/osx.py
+++ src/main/python/conveyor/platform/osx.py
@@ -37,13 +37,13 @@ DEFAULT_CONFIG_MAKERBOT_DRIVER_PROFILE_DIR = '/Library/MakerBot/s3g/profiles/'
 DEFAULT_CONFIG_MIRACLE_GRUE_EXE = '/Library/MakerBot/miracle_grue'
 
 
-DEFAULT_CONFIG_MIRACLE_GRUE_PROFILE_DIR = '/Library/MakerBot/src/main/miraclegrue/'
+DEFAULT_CONFIG_MIRACLE_GRUE_PROFILE_DIR = '/Library/MakerBot/miraclegrue/'
 
 
 DEFAULT_CONFIG_SKEINFORGE_FILE = '/Library/MakerBot/skeinforge/skeinforge_application/skeinforge.py'
 
 
-DEFAULT_CONFIG_SKEINFORGE_PROFILE_DIR = '/Library/MakerBot/src/main/skeinforge/'
+DEFAULT_CONFIG_SKEINFORGE_PROFILE_DIR = '/Library/MakerBot/skeinforge/'
 
 
 DEFAULT_CONFIG_SERVER_LOGGING_FILE = '/var/log/conveyor/conveyord.log'
