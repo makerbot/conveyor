273a13ae89c7d4df985ae94fcc7a677d67f13310
Matthew W. Samsonoff <matthew.samsonoff@makerbot.com>
2013-02-15 17:17:59 -0500

    Use the job's has_start_end flag when printing to file.

--- src/main/python/conveyor/recipe.py
+++ src/main/python/conveyor/recipe.py
@@ -492,7 +492,7 @@ class _StlRecipe(Recipe):
 
         with tempfile.NamedTemporaryFile(suffix='.gcode') as start_end_pathfp:
             start_end_path = start_end_pathfp.name
-        add_start_end = True
+        add_start_end = not self._job.has_start_end
         add_start_end_task = self._add_start_end_task(
             self._job.profile, self._job.slicer_settings, self._job.material_name,
             add_start_end, dualstrusion, processed_gcodepath, start_end_path)
